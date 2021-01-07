9aeaa01c51ab0df8fe12f1f1af66d088dd7533ac
Yan Maia <yandamasceno01@gmail.com>
2021-01-07 11:13:50 -0300

    preparacao da api

---
+++ src/api/endpoints/UserPost.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const UserPost = () => {
+  const [username, setUsername] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default UserPost

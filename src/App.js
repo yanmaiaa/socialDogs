61869fdf95bc067af8d18562a70bd9af4bd6b719
Yan Maia <yandamasceno01@gmail.com>
2021-01-11 10:39:02 -0300

    construcao da pagina inicial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import Api from './api/Api';
 import './App.css'
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+
+
 
 function App() {
   return (
     <div>
-     <Api/>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>}/> 
+    </Routes>
+    <Footer/>
+    </BrowserRouter>
     </div>
   );
 }

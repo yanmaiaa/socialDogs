35f4d5f728b59760dc86a91a9913b8baf9ebdd81
Yan Maia <yandamasceno01@gmail.com>
2021-01-11 11:15:15 -0300

    adicao do header

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
-
+import Login from './Components/Login/Login';
 
 
 function App() {
@@ -14,6 +14,7 @@ function App() {
     <Header/>
     <Routes>
       <Route path="/" element={<Home/>}/> 
+      <Route path="/login" element={<Login/>}/>
     </Routes>
     <Footer/>
     </BrowserRouter>

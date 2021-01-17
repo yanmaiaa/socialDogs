601e3fd6a9b100f6f5d3830bdc91ca8bc620f0bf
Yan Maia <yandamasceno01@gmail.com>
2021-01-17 17:11:42 -0300

    validacao de token

--- src/Hooks/useForm.js
+++ src/Hooks/useForm.js
@@ -3,7 +3,7 @@ import React from 'react';
 const types = {
   email: {
     regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-    message: 'Preencha um email válido!',
+    message: 'Preencha um email válido',
   },
 };
 

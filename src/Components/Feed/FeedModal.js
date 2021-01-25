23aa4a682e078965ffb8342b15e01fa03f3fc9b0
Yan Maia <yandamasceno01@gmail.com>
2021-01-25 02:14:12 -0300

    componente feed modal

--- src/Components/Feed/FeedModal.js
+++ src/Components/Feed/FeedModal.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import styles from './FeedModal.module.css';
+import useFetch from '../../Hooks/useFetch';
 
-const FeedModal = () => {
-  return <div></div>;
+const FeedModal = ({ photo }) => {
+  const { data, error, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    request(url, options);
+  }, [photo, request]);
+
+  return <div className={styles.modal}></div>;
 };
 
 export default FeedModal;

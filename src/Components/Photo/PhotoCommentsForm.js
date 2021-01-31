69df86a57e796fff5eb205b8889d9a377dacdee2
Yan Maia <yandamasceno01@gmail.com>
2021-01-31 15:42:26 -0300

    comentarios post component

---
+++ src/Components/Photo/PhotoCommentsForm.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { COMMENT_POST } from '../../api';
+import { ReactComponent as Enviar } from '../../Assets/enviar.svg';
+import useFetch from '../../Hooks/useFetch';
+
+export const PhotoCommentsForm = ({ id }) => {
+  const [comment, setComment] = React.useState('');
+  const { request, error } = useFetch();
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const { url, options } = COMMENT_POST(id, { comment });
+    await request(url, options);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <textarea
+        id="comment"
+        name="comment"
+        placeholder="Comente..."
+        value={comment}
+        onChange={({ target }) => setComment(target.value)}
+      />
+      <button>
+        <Enviar />
+      </button>
+    </form>
+  );
+};

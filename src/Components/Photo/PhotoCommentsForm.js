a9725dc9d7b2a32b3f142919e121464424718ad5
Yan Maia <yandamasceno01@gmail.com>
2021-02-01 23:11:02 -0300

    photo comment

--- src/Components/Photo/PhotoCommentsForm.js
+++ src/Components/Photo/PhotoCommentsForm.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { COMMENT_POST } from '../../api';
 import { ReactComponent as Enviar } from '../../Assets/enviar.svg';
 import useFetch from '../../Hooks/useFetch';
+import Error from '../Helper/Error';
 
-export const PhotoCommentsForm = ({ id }) => {
+export const PhotoCommentsForm = ({ id, setComments }) => {
   const [comment, setComment] = React.useState('');
   const { request, error } = useFetch();
 
   async function handleSubmit(event) {
     event.preventDefault();
     const { url, options } = COMMENT_POST(id, { comment });
-    await request(url, options);
+    const { response, json } = await request(url, options);
+    if (response.ok) {
+      setComment('');
+      setComments((comments) => [...comments, json]);
+    }
   }
 
   return (
@@ -25,6 +30,7 @@ export const PhotoCommentsForm = ({ id }) => {
       <button>
         <Enviar />
       </button>
+      <Error error={error} />
     </form>
   );
 };

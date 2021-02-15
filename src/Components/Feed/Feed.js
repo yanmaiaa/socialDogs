e7ff24dc13e232db63dd7632e41dcb1d065763cd
Yan Maia <yandamasceno01@gmail.com>
2021-02-15 00:16:52 -0300

    scroll infinito

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -2,15 +2,39 @@ import React from 'react';
 import FeedModal from './FeedModal';
 import FeedPhotos from './FeedPhotos';
 
-const Feed = () => {
+const Feed = ({ user }) => {
   const [modalPhoto, setModalPhoto] = React.useState(null);
+  const [pages, setPages] = React.useState([1]);
+
+  React.useEffect(() => {
+    function infiniteScroll() {
+      const scroll = window.scrollY;
+      const height = document.body.offsetHeight - window.innerHeight;
+      if ((scroll > height * 0, 75))
+        setPages((pages) => [...pages, pages.length + 1]);
+    }
+
+    window.addEventListener('wheel', infiniteScroll); //Caso não tenha barra de scroll e precisemos scrollar para ver outras fotos que não aparecem
+    window.addEventListener('scroll', infiniteScroll);
+    return () => {
+      window.removeEventListener('wheel', infiniteScroll);
+      window.removeEventListener('scroll', infiniteScroll);
+    };
+  }, []);
 
   return (
     <div>
       {modalPhoto && (
         <FeedModal photo={modalPhoto} setModalPhoto={setModalPhoto} />
       )}
-      <FeedPhotos setModalPhoto={setModalPhoto} />
+      {pages.map((page) => (
+        <FeedPhotos
+          key={page}
+          user={user}
+          page={page}
+          setModalPhoto={setModalPhoto}
+        />
+      ))}
     </div>
   );
 };

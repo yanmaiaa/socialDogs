8d05dcfa5efb0f7bd877b1b21a7f1a3a73f65c25
Yan Maia <yandamasceno01@gmail.com>
2021-02-21 23:58:04 -0300

    finalizacao do fotos component

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -11,7 +11,7 @@ const PhotoContent = ({ data, single }) => {
   const { photo, comments } = data;
 
   return (
-    <div className={`${styles.photo} ${single && styles.single}`}>
+    <div className={`${styles.photo} ${single ? styles.single : ''}`}>
       <div className={styles.img}>
         <Image src={photo.src} alt={photo.title} />
       </div>
@@ -35,7 +35,7 @@ const PhotoContent = ({ data, single }) => {
           </ul>
         </div>
       </div>
-      <PhotoComments id={photo.id} comments={comments} />
+      <PhotoComments single={single} id={photo.id} comments={comments} />
     </div>
   );
 };

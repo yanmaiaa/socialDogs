3d50c84fd25ec3e8882e435e8468a51e2dc724e3
Yan Maia <yandamasceno01@gmail.com>
2021-02-28 01:49:27 -0300

    pagina perdeu a senha

--- src/Components/Login/LoginPasswordLost.js
+++ src/Components/Login/LoginPasswordLost.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React from 'react';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
+import useFetch from '../../Hooks/useFetch';
+import { PASSWORD_LOST } from '../../api';
 
 const LoginPasswordLost = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const login = useForm();
+  const { data, loading, error, request } = useFetch();
 
-export default LoginPasswordLost
+  function handleSubmit(event) {
+    //Fez uma requisição e por isso irá usar um Fetch
+    event.preventDefault();
+    const { url, options } = PASSWORD_LOST({
+      login: login.value,
+      url: 'http://localhost:3000/login/perdeu',
+    });
+    request(url, options);
+  }
+
+  return (
+    <section>
+      <h1 className="title">Perdeu a senha?</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Email / Usuário" type="text" name="login" {...login} />
+        {login.value}
+        <Button>Enviar Email</Button>
+      </form>
+    </section>
+  );
+};
+
+export default LoginPasswordLost;
